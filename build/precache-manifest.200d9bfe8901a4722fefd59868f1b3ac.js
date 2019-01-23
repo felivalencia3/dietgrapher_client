8cb17d7df1763782ba7b00dc362bd9d6077d6fb7
Felipe Valencia <fvalencia@vc-soft.com>
2019-01-23 09:11:13 -0500

    Build Complete

---
+++ build/precache-manifest.200d9bfe8901a4722fefd59868f1b3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3f85eb4bbc5f5f7a05a",
+    "url": "/static/css/main.d9cd608d.chunk.css"
+  },
+  {
+    "revision": "d3f85eb4bbc5f5f7a05a",
+    "url": "/static/js/main.d3f85eb4.chunk.js"
+  },
+  {
+    "revision": "8c7d5b1e38e0f37bdd03",
+    "url": "/static/css/1.cf944d6a.chunk.css"
+  },
+  {
+    "revision": "8c7d5b1e38e0f37bdd03",
+    "url": "/static/js/1.8c7d5b1e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f61ac6d5cca5cec1a2f4f778bd13019",
+    "url": "/index.html"
+  }
+];
